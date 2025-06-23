25176c5fb019606b1f3ca8f68535148db93b9178
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-06-23 19:09:36 +0930

    built fully scalable and reusable button component using slot, cva and cn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,17 @@ import { ButtonVariants } from "../cva/ButtonVariants";
 import { cn } from "../utis/cn";
 import {Slot} from "@radix-ui/react-slot"
 
-export function button(){
+export function button(className,asChild,variant,size,...props){
+    const Comp=asChild? Slot:"button"
     return(
         <>
-        
+        <Comp className={cn(
+            ButtonVariants({variant,size}),
+            className
+        )} {...props}>
+
+        </Comp>
+
         </>
     )
 }
