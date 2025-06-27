08efe6bb3427885967c51cd2a006b8cd45830c34
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-06-27 22:36:23 +0930

    made it responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,16 @@ import { Button } from './Button'
 import { NavLink } from 'react-router'
 import { Link } from 'react-router'
 import { HugeiconsIcon } from '@hugeicons/react';
+import { Menu04Icon } from '@hugeicons/core-free-icons'
+import {Menu03Icon} from '@hugeicons/core-free-icons'
 import { ArrowDown01Icon } from '@hugeicons/core-free-icons';
+import { useEffect, useState } from 'react'
 
 export function Navbar() {
+    const [showmenu,setshowmenu]=useState(false)
+    const togglemenu=()=>{
+        setshowmenu(prev=>!prev)
+    }
     const DropdownLinks = [
         {
             name: "Our Services",
@@ -25,11 +32,11 @@ export function Navbar() {
 
     return (
         <>
-            <nav className="h-10 flex justify-around items-center py-8">
+            <nav className="h-10 flex md:justify-around items-center sm:py-8 py-10 justify-between px-3 md:px-0 shadow-md">
                 <div>
                     <img src={logo} alt="site logo" className='w-30 ' />
                 </div>
-                <div className='flex gap-2 items-center'>
+                <div className='md:flex gap-2 items-center hidden '>
                     <Button asChild >
                         <NavLink to={"/"} onClick={() => window.scrollTo(0, 0)}>Home</NavLink>
                     </Button>
@@ -51,7 +58,7 @@ export function Navbar() {
                             strokeWidth={2}
                             className="ml-1 transition-transform duration-300 group-hover:rotate-180"
                         />
-                        <div className="absolute w-[150px]  top-10 bg-gray-400 py-4 px-2 ">
+                        <div className="absolute w-[150px]  top-12 bg-white py-4 px-2 shadow-md">
                             {DropdownLinks.map(({ name, link }) => (
                                 <div className='leading-loose hover:bg-blue-200 rounded-md'>
                                     <Button asChild>
@@ -67,16 +74,41 @@ export function Navbar() {
 
 
                 </div>
-                <Button variant="primary">
-                    <span>
+                <div className='flex items-center gap-5'>
+                    <Button variant="primary">
+                        <span>
 
-                        Book Now
-                    </span>
+                            Book Now
+                        </span>
 
 
-                </Button>
+
+
+                    </Button>
+                    {
+                        showmenu?(
+                    
+                    <HugeiconsIcon icon={Menu03Icon}
+                       strokeWidth={3}
+                    onClick={togglemenu}
+                    className="hover:cursor-pointer"
+                    />
+                    
+                )
+                    :(
+                       
+                        <HugeiconsIcon 
+                    icon={Menu04Icon} 
+                    strokeWidth={3}
+                    onClick={togglemenu}
+                    className="hover:cursor-pointer"
+                    />
+                    )
+}
+                </div>
 
             </nav>
         </>
     )
 }
+
