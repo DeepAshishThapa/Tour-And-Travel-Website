94e94bbb16f924eac8c36a1c59e6b10991c02216
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-07-04 19:53:39 +0930

    fully made responsive home hero component

--- src/components/Homehero.jsx
+++ src/components/Homehero.jsx
@@ -15,21 +15,21 @@ return(
         <div className="h-[60vh] ">
 
             <div className="h-full flex items-center ">
-                <div className="grid grid-cols-1 gap-4 text-white w-[80%] m-auto 2xl:w-300 ">
+                <div className="grid grid-cols-1 gap-4 text-white w-[95%] m-auto md  2xl:w-300 ">
                     <div>
                         <p>Our Packages</p>
                         <h1 className="text-4xl font-bold">Search Your Destination</h1>
                     </div>
-                    <div className="w-full h-35 bg-white rounded-md px-4 py-4 relative flex  justify-center gap-5 text-gray-500">
-                        <div className="w-[32%]">
+                    <div className="w-full sm:h-35 h-auto bg-white rounded-md px-4 py-4 relative flex-col flex sm:flex-row justify-center gap-5 text-gray-500">
+                        <div className="sm:w-[32%] w-[100%]">
                             <h1 className="">Search Your Destination</h1>
                             <Inputbox className="w-[100%]" type="text" placeholder="Dubai" />
                         </div>
-                        <div className="w-[32%]">
+                        <div className="w-[100%] sm:w-[32%]">
                             <h1 className="">Date</h1>
                             <Inputbox className="w-[100%]" type="date" />
                         </div>
-                        <div className="w-[32%]">
+                        <div className="sm:w-[32%] w-[100%]">
                             <div className="flex justify-between">
                             <h1 className="">Max Price</h1>
                             <label for="slide">
@@ -56,7 +56,7 @@ return(
 
 
 
-                        <Button variant="primary" size="md" className="rounded-full absolute top-30">
+                        <Button variant="primary" size="md" className="rounded-full absolute sm:top-30 top-64 ">
                             Search Now
 
                         </Button>
