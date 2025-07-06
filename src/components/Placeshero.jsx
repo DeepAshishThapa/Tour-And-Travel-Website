dccf4c0521f32ddb278e53e351fc866de0a3814b
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-07-06 23:12:07 +0930

    wrapped card component insde it

--- src/components/Placeshero.jsx
+++ src/components/Placeshero.jsx
@@ -1,5 +1,7 @@
 import { Card } from "./Card";
-
+import boat from '../assets/boat.jpg'
+import { HugeiconsIcon } from '@hugeicons/react';
+import { MapPinpoint01Icon } from '@hugeicons/core-free-icons';
 export const Placeshero = () => {
     return (
         <>
@@ -11,16 +13,39 @@ export const Placeshero = () => {
                 </div>
 
                 <div className="pt-10 grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-4 ">
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                    <Card>
+                        <img src={boat} alt="" className="h-[55%] w-full object-cover" />
+                        <div className="flex flex-col p-3 gap-2">
+                            <h1 className="font-bold text-xl">Boat tour</h1>
+                            <div className="flex items-center gap-1.5">
+                                 <HugeiconsIcon icon={MapPinpoint01Icon}
+                                    size={18}
+                                    color="#000000"
+                                    className="text-gray-500"
+
+                                />
+                                <div className="text-gray-500">USA</div>
+                               
+                            </div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.     </p>
+                            <div className="h-[1.3px] bg-gray-200"></div>
+
+                            <ul className="flex items-center justify-between"> 
+                                <li>Cultural Relax</li>
+                                <li className="font-bold text-2xl">$100</li>
+                            </ul>
+
+                        </div>
+                    </Card>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
 
                 </div>
 
-                
+
 
 
 
