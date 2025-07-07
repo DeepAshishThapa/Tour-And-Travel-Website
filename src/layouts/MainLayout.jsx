5679a41fd535619188c627841bfbd3f6a99d5992
DeepAshishThapa <dipashishthapa98@gmail.com>
2025-07-07 14:00:13 +0930

    added footer in main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router"
 import { Navbar } from "../components/Navbar"
+import {Footer} from '../components/footer'
 
 export function MainLayout() {
     return (
         <>
+
+
             
             
             <Navbar />
             <Outlet />
+            <Footer/>
+            
+
 
 
         </>
